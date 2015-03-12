295292142211fbc1beed8fcbd0883be0af5c708b
Sheldon <Sheldon@Sheldon>
2015-03-12 18:37:31 -0400

    Login GUI

---
+++ src/shha/SHHA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shha;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Sheldon Gray
+ */
+
+
+public class SHHA extends JFrame {
+
+   
+    public static void main(String[] args) {
+      loginGUI2 gui = new loginGUI2();
+      gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      gui.setVisible(true);
+    }
+    
+}
